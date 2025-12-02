2d7506eb0075bb33bf1d07c90ba5cfea7428907f
Sujaybk11 <sujaybk11@gmail.com>
2025-12-02 19:44:24 +0530

    Fix image imports for Vite production build

---
+++ frontend/src/assets/images.js
@@ -0,0 +1,27 @@
+import food_1 from './frontend_assets/food_1.png'
+import food_2 from './frontend_assets/food_2.png'
+import food_3 from './frontend_assets/food_3.png'
+import food_4 from './frontend_assets/food_4.png'
+import food_5 from './frontend_assets/food_5.png'
+import food_6 from './frontend_assets/food_6.png'
+import food_7 from './frontend_assets/food_7.png'
+import food_8 from './frontend_assets/food_8.png'
+import food_9 from './frontend_assets/food_9.png'
+import food_10 from './frontend_assets/food_10.png'
+import food_11 from './frontend_assets/food_11.png'
+import food_12 from './frontend_assets/food_12.png'
+import food_13 from './frontend_assets/food_13.png'
+import food_14 from './frontend_assets/food_14.png'
+import food_15 from './frontend_assets/food_15.png'
+import food_16 from './frontend_assets/food_16.png'
+import food_17 from './frontend_assets/food_17.png'
+import food_18 from './frontend_assets/food_18.png'
+import food_19 from './frontend_assets/food_19.png'
+import food_20 from './frontend_assets/food_20.png'
+
+export const images = {
+  food_1, food_2, food_3, food_4, food_5,
+  food_6, food_7, food_8, food_9, food_10,
+  food_11, food_12, food_13, food_14, food_15,
+  food_16, food_17, food_18, food_19, food_20
+}
